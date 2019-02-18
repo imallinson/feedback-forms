d9fff4cf634b8cd231460aaa75fc2cab9a0cd871
imallinson <imallinson1001@gmail.com>
2019-02-18 14:58:44 +0000

    install shell script

---
+++ install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/imallinson/feedback-forms-accounts
+git clone https://github.com/imallinson/feedback-forms-display
+git clone https://github.com/imallinson/feedback-forms-submit
+git clone https://github.com/imallinson/feedback-forms-consumer
+git clone https://github.com/imallinson/feedback-forms-front
+
+docker-compose up -d
