39a155a406bae978778998e9e208dbdb46a6ef9a
imallinson <imallinson1001@gmail.com>
2019-02-20 12:46:18 +0000

    removing display microservice

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 git clone https://github.com/imallinson/feedback-forms-accounts
-git clone https://github.com/imallinson/feedback-forms-display
 git clone https://github.com/imallinson/feedback-forms-submit
 git clone https://github.com/imallinson/feedback-forms-consumer
 git clone https://github.com/imallinson/feedback-forms-front
