8080bbf4047dffeec13b6a46cdd1e59b94018818
imallinson <45758809+imallinson@users.noreply.github.com>
2019-02-27 12:55:01 +0000

    fix

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ git clone https://github.com/imallinson/feedback-forms-consumer
 git clone https://github.com/imallinson/feedback-forms-front
 git clone https://github.com/imallinson/feedback-forms-gateway
 git clone https://github.com/imallinson/feedback-forms-retriever
-git clone https://github.com/imallinson/feedback-forms-cohort
+git clone https://github.com/imallinson/feedback-forms-cohorts
 
 docker-compose up -d
